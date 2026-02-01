b8331ebd2ac4506ca5cbeba69c405e97201b8b40
muckho2728 <haiha3639@gmail.com>
2026-02-02 03:52:15 +0700

    screen intro/setting camonvidaden

--- lib/core/audio_manager.dart
+++ lib/core/audio_manager.dart
@@ -1,11 +1,14 @@
 import 'package:audioplayers/audioplayers.dart';
-import 'package:flutter/foundation.dart';
 
 class AudioManager {
   //nhạc nền
   static final AudioPlayer _bgPlayer = AudioPlayer();
   //hiệu ứng
   static final AudioPlayer _sfxPlayer = AudioPlayer();
+
+  // Trạng thái nhạc
+  static bool isMusicOn = true;
+  static double musicVolume = 0.5;
 
   //NHẠC NỀN
   static Future<void> playBackground() async {
@@ -14,9 +17,9 @@ class AudioManager {
     try {
       await _bgPlayer.setReleaseMode(ReleaseMode.loop);
       await _bgPlayer.play(AssetSource('audios/trinh.m4a'));
-      await _bgPlayer.setVolume(0.5);
+      await _bgPlayer.setVolume(musicVolume);
     } catch (e) {
-      debugPrint("Chưa có file nhạc nền: $e");
+      print("Chưa có file nhạc nền: $e");
     }
   }
 
@@ -24,7 +27,27 @@ class AudioManager {
     try {
       await _bgPlayer.stop();
     } catch (e) {
-      debugPrint("Lỗi dừng nhạc: $e");
+      print("Lỗi dừng nhạc: $e");
+    }
+  }
+
+  // BẬT/TẮT NHẠC
+  static Future<void> toggleMusic(bool isOn) async {
+    isMusicOn = isOn;
+    if (isOn) {
+      await playBackground();
+    } else {
+      await stopBackground();
+    }
+  }
+
+  // ĐIỀU CHỈNH ÂM LƯỢNG
+  static Future<void> setMusicVolume(double volume) async {
+    musicVolume = volume;
+    try {
+      await _bgPlayer.setVolume(volume);
+    } catch (e) {
+      print("Lỗi set volume: $e");
     }
   }
 
@@ -37,8 +60,7 @@ class AudioManager {
       await _sfxPlayer.play(AssetSource('audios/$fileName'));
       await _sfxPlayer.setVolume(1.0);
     } catch (e) {
-      debugPrint("Thiếu file SFX ($fileName): $e");
+      print("Thiếu file SFX ($fileName): $e");
     }
   }
 }
-
